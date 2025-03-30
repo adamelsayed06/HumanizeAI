f9b1e1bbc4dc39c5bd46f5378b068396af7150df
Adam Elsayed <adamelsayyed06@gmail.com>
2025-03-29 20:33:23 -0400

    bug fix

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,7 @@ from openai import OpenAI
 app = Flask(__name__)
 CORS(app)
 
-load_dotenv
+load_dotenv()
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 client = OpenAI(api_key=OPENAI_API_KEY)
 
