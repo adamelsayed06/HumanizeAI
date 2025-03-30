457a05186a8831699be36f69df4afe4211083ba9
Adam Elsayed <adamelsayyed06@gmail.com>
2025-03-29 20:11:21 -0400

    load api key

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,17 @@
 import json
 from dotenv import load_dotenv
+import os
 
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from openai import OpenAI
 
 app = Flask(__name__)
 CORS(app)
+
+load_dotenv
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+
+@app.route("/humanized", methods=["POST"])
+def humanize_text():
+
